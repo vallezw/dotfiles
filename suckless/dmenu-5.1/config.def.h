ef2db4c03c2562c70c2659961e496b444482e305
Valentin Zwerschke <v.zwerschke@mail.de>
2022-07-04 19:37:06 +0200

    Patching in dwm and dmenu

--- suckless/dmenu-5.1/config.def.h
+++ suckless/dmenu-5.1/config.def.h
@@ -9,9 +9,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#a89984", "#282828" },
+	[SchemeSel] = { "#a89984", "#cc241d" },
+	[SchemeOut] = { "#a89984", "#689d6a" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
--- suckless/dwm-6.3/config.h
+++ suckless/dwm-6.3/config.h
@@ -12,11 +12,34 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+
+// Gruvbox theme colors
+static const char norm_fg[] = "#a89984";
+static const char norm_bg[] = "#282828";
+static const char norm_border[] = "#928374";
+
+static const char sel_fg[] = "#282828";
+static const char sel_bg[] = "#d65d0e";
+static const char sel_border[] = "#a89984";
+
+static const char urg_fg[] = "#a89984";
+static const char urg_bg[] = "#cc241d";
+static const char urg_border[] = "#b8bb26";
+
+static const char title_fg[] = "#b8bb26";
+static const char title_bg[] = "#3A3A3A";
+
+static const char col_borderbar[]   = "#75715e";
+
+
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+    /*               fg           bg         border                         */
+    [SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
+    [SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
+// [SchemeUrg] =  { urg_fg,      urg_bg,    urg_border },
+  // [SchemeTitle] = { title_fg,   title_bg,  norm_border },
 };
+
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
