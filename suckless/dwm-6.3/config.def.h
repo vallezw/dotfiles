57abfb248de30e13780bd9e036364d2279f6805a
Valentin Zwerschke <v.zwerschke@mail.de>
2022-07-08 21:52:02 +0200

    Add flameshot screenshot cmd at mod shift s

--- suckless/dwm-6.3/config.def.h
+++ suckless/dwm-6.3/config.def.h
@@ -92,6 +92,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask, 		XK_s, 	   spawn, 	    SHCMD("flameshot gui") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
