e9d3b899f60894b8eae3bc5fe1c1a99cb8eeb64b
Valentin Zwerschke <v.zwerschke@mail.de>
2022-07-08 22:41:27 +0200

    Add rules

--- suckless/dwm-6.3/config.def.h
+++ suckless/dwm-6.3/config.def.h
@@ -53,9 +53,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      		instance    title       tags mask     isfloating   monitor */
+	{ "TelegramDesktop", 	NULL,       NULL,       1 << 7,            0,           -1 },
+	{ "discord",  		NULL,       NULL,       1 << 7,            0,           -1 },
+	{ "Spotify",  		NULL,       NULL,       1 << 8,            0,           -1 },
 };
 
 /* layout(s) */
