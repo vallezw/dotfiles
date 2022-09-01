9ab9f97431dc08cfdf1aa5f46ff86fb8a32d414b
Valentin Zwerschke <v.zwerschke@mail.de>
2022-09-01 16:26:54 +0200

    Make font sizes bigger by 1

--- suckless/dwm-6.3/config.def.h
+++ suckless/dwm-6.3/config.def.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 4;        /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=11" };
+static const char dmenufont[]       = "monospace:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
