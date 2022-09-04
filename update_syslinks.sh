02faf3508123b69af4fe54f6bd46eaee3d40aed1
Valentin Zwerschke <v.zwerschke@mail.de>
2022-09-04 12:17:18 +0200

    Remove comment

--- update_syslinks.sh
+++ update_syslinks.sh
@@ -2,7 +2,7 @@
 # This short script is for creating/updating the syslinks for basic files such as .basrc and .xinitrc
 echo This script will force overwrite your current .xinitrc and .bashrc files. 
 read -p "Are you sure that you want to run it anyways? [Y/n] " -n 1 -r
-echo    # (optional) move to a new line
+echo 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     ln -s -f ~/dotfiles/.xinitrc ~/.xinitrc
