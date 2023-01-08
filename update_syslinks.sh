2ee7ef9389b14eff9d65538cb2153e2582e15ced
Valentin Zwerschke <vallezw@gmail.com>
2023-01-08 23:16:51 +0100

    Add git config and quantco

--- update_syslinks.sh
+++ update_syslinks.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#/bin/sh
 # This short script is for creating/updating the syslinks for basic files such as .basrc and .xinitrc
 echo This script will force overwrite your current .xinitrc and .bashrc files. 
 read -p "Are you sure that you want to run it anyways? [Y/n] " -n 1 -r
@@ -7,6 +7,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]
 then
     ln -s -f ~/dotfiles/.xinitrc ~/.xinitrc
 	ln -s -f ~/dotfiles/.bashrc ~/.bashrc
-	ln -s -f ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim 
+	ln -s -f ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+	ln -s -f ~/dotfiles/git/gitconfig ~/.gitconfig
 	echo Updated sys links successfullly
 fi
